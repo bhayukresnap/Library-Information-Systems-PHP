4ac860d10d6c009c2e9cc89d900c44f65e84726e
bhayukresnap <48900624+bhayukresnap@users.noreply.github.com>
2021-01-07 16:42:20 +0700

    .

--- public/admin/test.php
+++
@@ -1,6 +0,0 @@
-<?php 
-require_once($_SERVER['DOCUMENT_ROOT'].'/src/AdminSession.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/src/Controller/Order.php');
-$tag = new Order();
-print($tag->inserts());
-?>
---
+++ public/test.php
@@ -0,0 +1,5 @@
+<?php 
+	$test = array("test"=>"");
+print_r(strlen($test['test']));
+
+ ?>
